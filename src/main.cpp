45795c1fc7cd4a76f2f8099eee530be0470861b5
Uproxide <153382689+Uproxide@users.noreply.github.com>
2024-03-28 03:34:29 -0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,88 @@
-#include <Geode/modify/GJDifficultySprite.hpp>
-#include <fmt/format.h>
+#include <Geode/Geode.hpp>
 
 using namespace geode::prelude;
 
-class $modify(GJDifficultySprite) {
-    void updateFeatureStateFromLevel(GJGameLevel* level) {
-        updateCustomDifficultyFrame(level->m_stars);
-        GJDifficultySprite::updateFeatureStateFromLevel(level);
-    }
+#include <Geode/modify/LevelInfoLayer.hpp>
+#include <Geode/modify/LevelCell.hpp>
 
-    void updateDifficultyFrame(int frame, GJDifficultyName name) {
-        // Workaround for positions being updated.
-        if (!m_fields->m_customDifficultyFrame) {
-            GJDifficultySprite::updateDifficultyFrame(frame, name);
-        }
-    }
+class $modify(LevelInfoLayer) {
+	bool init(GJGameLevel* p0, bool p1) {
+		if (!LevelInfoLayer::init(p0, p1)) {
+			return false;
+		}
 
-    void updateCustomDifficultyFrame(int stars) {
-        auto* spriteFrameCache = CCSpriteFrameCache::sharedSpriteFrameCache();
-        bool legacyIcons = Mod::get()->getSettingValue<bool>("legacy-icons");
+		int starCount = p0->m_stars.value();
 
-        m_fields->m_customDifficultyFrame = false;
-        
-        std::string suffix = legacyIcons ? "_Legacy" : "";
-        std::string spriteFrameName = fmt::format(
-            "MD_Difficulty{:02}{}.png"_spr,
-            stars,
-            suffix
-        );
+		cocos2d::CCPoint difficultyPos = m_difficultySprite->getPosition();
+		int zOrder = m_difficultySprite->getZOrder();
 
-        auto* spriteFrame = spriteFrameCache->spriteFrameByName(
-            spriteFrameName.c_str()
-        );
 
-        if (spriteFrame != nullptr) {
-            m_fields->m_customDifficultyFrame = true;
-            setDisplayFrame(spriteFrame);
-        }
-    }
+		auto casualSpr = CCSprite::create("MD_Difficulty04.png"_spr);
+		auto difficultSpr = CCSprite::create("MD_Difficulty07.png"_spr);
+		auto cruelSpr = CCSprite::create("MD_Difficulty09.png"_spr);
 
-    bool m_customDifficultyFrame;
+		casualSpr->setZOrder(zOrder);
+		difficultSpr->setZOrder(zOrder);
+		cruelSpr->setZOrder(zOrder);
+
+
+
+		if (starCount == 4) {
+			casualSpr->setPosition(difficultyPos);
+			this->addChild(casualSpr);
+			m_difficultySprite->setOpacity(0);
+		} else if (starCount == 7) {
+			difficultSpr->setPosition(difficultyPos);
+			this->addChild(difficultSpr);
+			m_difficultySprite->setOpacity(0);
+		} else if (starCount == 9) {
+			cruelSpr->setPosition(difficultyPos);
+			this->addChild(cruelSpr);
+			m_difficultySprite->setOpacity(0);
+		}
+
+		return true;
+	}
+};
+
+class $modify(LevelCell) {
+	void loadFromLevel(GJGameLevel* p0) {
+		LevelCell::loadFromLevel(p0);
+
+		int starCount = p0->m_stars.value();
+
+		auto difficultyNode = m_mainLayer->getChildByID("difficulty-container");
+
+		if (difficultyNode) {
+			GJDifficultySprite* difficultySpr = static_cast<GJDifficultySprite*>(difficultyNode->getChildByID("difficulty-sprite"));
+
+			cocos2d::CCPoint difficultyPos = difficultySpr->getPosition();
+			int zOrder = difficultySpr->getZOrder();
+
+
+			auto casualSpr = CCSprite::create("MD_Difficulty04.png"_spr);
+			auto difficultSpr = CCSprite::create("MD_Difficulty07.png"_spr);
+			auto cruelSpr = CCSprite::create("MD_Difficulty09.png"_spr);
+
+			casualSpr->setZOrder(zOrder);
+			difficultSpr->setZOrder(zOrder);
+			cruelSpr->setZOrder(zOrder);
+
+			if (starCount == 4) {
+				casualSpr->setPosition(difficultyPos);
+				difficultyNode->addChild(casualSpr);
+				difficultySpr->setOpacity(0);
+			} else if (starCount == 7) {
+				difficultSpr->setPosition(difficultyPos);
+				difficultyNode->addChild(difficultSpr);
+				difficultySpr->setOpacity(0);
+			} else if (starCount == 9) {
+				cruelSpr->setPosition(difficultyPos);
+				difficultyNode->addChild(cruelSpr);
+				difficultySpr->setOpacity(0);
+			}
+		} else {
+			// log::info("difficultyNode no no exist");
+		}
+	}
 };
